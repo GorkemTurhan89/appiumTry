12a5b6dbb92302f4f51920d05540aab4be1bfad7
Gooork3m <gorkemturhan41@gmail.com>
2023-09-07 00:35:50 +0300

    ap2

--- src/test/java/Appium/Appium02.java
+++ src/test/java/Appium/Appium02.java
@@ -3,7 +3,6 @@ package Appium;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -12,7 +11,7 @@ import java.net.URL;
 
 import static org.testng.AssertJUnit.assertTrue;
 
-public class Appium01 {
+public class Appium02 {
     @Test
     public void test() throws MalformedURLException, InterruptedException {
         DesiredCapabilities capabilities= new DesiredCapabilities();
@@ -27,6 +26,14 @@ capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
 capabilities.setCapability("aapActivity","com.davemac327.gesture.tool/com.devamac327.gesture.tool.GestureBuilderActivity");
 AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         System.out.println("app yuklendi");
+driver.findElementById("com.android.permisssioncontroller:id/continue_button").click();
+Thread.sleep(3000);
 
+MobileElement okButton = driver.findElementByXPath("//android.widget.Button[@text='OK']");
+okButton.click();
+        System.out.println("Permittion Granted Blo");
+MobileElement homeScreenTitle = driver.findElementById("android:id/title");
+assertTrue(homeScreenTitle.isDisplayed());
+        System.out.println("Home pagee openned");
     }
 }
